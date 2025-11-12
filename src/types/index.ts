2d905321cbb4685b1db6c3bc67569a2444d164c1
ybt <22301106@bjtu.edu.cn>
2025-11-12 13:28:26 +0800

    feat(src and example/error): 新增错误处理：网络错误、超时错误、非200错误
    - 新增错误处理相关工具函数

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,4 +20,17 @@ export interface AxiosRequestConfig {
   data?: any
   params?: any
   headers?: any
+  responseType?: XMLHttpRequestResponseType
+  timeout?: number
 }
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {}
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,19 +1,67 @@
-import { AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
 
-export default function xhr(config: AxiosRequestConfig) {
-  const { data, url, method = 'get', headers } = config
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve, reject) => {
+    const { data, url, method = 'get', headers, responseType, timeout } = config
 
-  const request = new XMLHttpRequest()
+    const request = new XMLHttpRequest()
 
-  request.open(method.toUpperCase(), url, true) // 第三个参数表示开启异步
+    if (responseType) {
+      request.responseType = responseType
+    }
+    if (timeout) {
+      request.timeout = timeout
+    }
 
-  Object.keys(headers).forEach(name => {
-    if (data === null && name.toLowerCase() === 'content-type') {
-      delete headers[name]
-    } else {
-      request.setRequestHeader(name, headers[name])
+    request.open(method.toUpperCase(), url, true) // 第三个参数表示开启异步
+
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+
+      if (request.status === 0) {
+        return
+      }
+
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      handleResponse(response)
+    }
+
+    request.onerror = function handleError() {
+      reject(new Error('Network Error'))
+    }
+
+    request.ontimeout = function handleTimeout() {
+      reject(new Error(`Timeout of ${timeout} ms exceeded`))
+    }
+
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    request.send(data)
+
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(new Error(`Request failed with status code ${response.status}`))
+      }
     }
   })
-
-  request.send(data)
 }
