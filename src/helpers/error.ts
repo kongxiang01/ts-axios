e18d5f1578c6345238740bb382044ee6c90eb3aa
ybt <22301106@bjtu.edu.cn>
2025-11-12 22:47:15 +0800

    feat(src and example/error): 错误信息增强
    新增 AxiosError 接口和 AxiosError 类，将更多信息放入 error 中；将axios独立出来，从而将types也导出。

---
+++ src/helpers/error.ts
@@ -0,0 +1,39 @@
+import { AxiosRequestConfig, AxiosResponse } from '../types'
+
+export class AxiosError extends Error {
+  isAxiosError: boolean
+  config: AxiosRequestConfig
+  code?: string | null
+  request?: any
+  response?: AxiosResponse
+
+  constructor(
+    message: string,
+    config: AxiosRequestConfig,
+    code?: string | null,
+    request?: any,
+    response?: AxiosResponse
+  ) {
+    super(message)
+
+    this.config = config
+    this.code = code
+    this.request = request
+    this.response = response
+    this.isAxiosError = true
+
+    // typescript 的一个坑，继承内置类时会调用不了类的方法并且对实例使用instanceof返回false，需要显示设置原型
+    Object.setPrototypeOf(this, AxiosError.prototype)
+  }
+}
+
+export function createError(
+  message: string,
+  config: AxiosRequestConfig,
+  code?: string | null,
+  request?: any,
+  response?: AxiosResponse
+) {
+  const error = new AxiosError(message, config, code, request, response)
+  return error
+}
