baca230418f82618bfb108c63d89383123880565
ybt <22301106@bjtu.edu.cn>
2025-11-13 11:16:34 +0800

    feat(src and examples): 实现axios转为混合对象；实现request函数重载
    导出的axios既可作为函数直接调用，又可通过.调用get等方法；重载后支持url、config传入参数

---
+++ src/core/despatchRequest.ts
@@ -0,0 +1,38 @@
+import { transformRequest, transformResponse } from '../helpers/data'
+import { processHeaders } from '../helpers/headers'
+import { buildURL } from '../helpers/url'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
+import xhr from './xhr'
+
+export default function dispatchRequest(config: AxiosRequestConfig): AxiosPromise {
+  processConfig(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
+}
+
+// 以下都是辅助函数
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url!, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
+}
