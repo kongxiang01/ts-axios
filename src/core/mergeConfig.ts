c5865c14674e7082974faf94e5dcf5d6be4d0c12
ybt <22301106@bjtu.edu.cn>
2025-11-16 14:13:12 +0800

    feat(axios.ts and types): 实现axios.create静态方法，该方法返回一个新的Axios实例

--- src/core/mergeConfig.ts
+++ src/core/mergeConfig.ts
@@ -40,7 +40,7 @@ stratKeysDeepMerge.forEach(key => {
 })
 export default function mergeConfig(
   config1: AxiosRequestConfig,
-  config2: AxiosRequestConfig
+  config2?: AxiosRequestConfig
 ): AxiosRequestConfig {
   if (!config2) {
     config2 = {}
