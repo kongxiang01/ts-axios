11dc9a9f18a9d3b1201bd5d335bc6c524b52111f
ybt <22301106@bjtu.edu.cn>
2025-11-19 13:43:14 +0800

    feat: 新增axios静态方法(all, spread)和成员方法(getUri)

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -1,6 +1,6 @@
 import { RejectedFn } from './../types/index'
 import { AxiosPromise, AxiosRequestConfig, AxiosResponse, Method, ResolvedFn } from '../types'
-import dispatchRequest from './despatchRequest'
+import dispatchRequest, { transformURL } from './despatchRequest'
 import InterceptorManager from './interceptorManager'
 import mergeConfig from './mergeConfig'
 
@@ -88,6 +88,11 @@ export default class Axios {
 
   patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
     return this._requestMethodWithData('patch', url, data, config)
+  }
+
+  getUri(config?: AxiosRequestConfig): string {
+    config = mergeConfig(this.defaults, config)
+    return transformURL(config)
   }
 
   _requestMethodWithoutData(
